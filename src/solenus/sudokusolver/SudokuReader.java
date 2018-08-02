4ad972830a0dd5bf2d4466857425e79128ea70f6
CSDragon <CSDragon31@hotmail.com>
2018-08-01 23:45:01 -0500

    level two logic

---
+++ src/solenus/sudokusolver/SudokuReader.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solenus.sudokusolver;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Chris
+ */
+public class SudokuReader 
+{
+    
+    public static int[][] test()
+    {
+        try
+        {
+        BufferedReader in = new BufferedReader(new FileReader("SudokuSolver/assets/sampleEasy.txt"));
+        return makeNumGrid(in);
+        }
+        catch(Exception E)
+        {
+                        System.out.println("oh");
+
+        }
+        return null;
+    }
+    
+    
+    public static int[][] makeNumGrid(BufferedReader in) throws IOException
+    {
+        int[][] ret = new int[9][9];
+        
+        for(int i = 0; i<9; i++)
+        {
+            char[] line = new char[9];
+            line = in.readLine().toCharArray();
+            for(int j = 0; j<9; j++)
+                ret[i][j] =  line[j]-48; 
+        }
+        
+        return ret;
+    }
+}
--- src/solenus/sudokusolver/SudokuSolver.java
+++ src/solenus/sudokusolver/SudokuSolver.java
@@ -24,23 +24,37 @@ public class SudokuSolver
         for(int k = 0; k< 81; k++)
             nums[k%9][k/9] = (int)(Math.random()*10);
         
-        //sample
-        nums = new int[][]{{0,1,9,0,5,0,7,0,0}
-                          ,{0,7,0,1,9,0,0,0,8}
-                          ,{0,0,0,3,0,6,2,0,9}
-                          ,{0,0,5,0,0,2,0,0,3}
-                          ,{7,0,0,0,4,0,0,0,1}
-                          ,{4,0,0,8,0,0,6,0,0}
-                          ,{9,0,4,6,0,7,0,0,0}
-                          ,{6,0,0,0,2,3,0,8,0}
-                          ,{0,0,7,0,1,0,3,9,0}};
+        //sample medium
+        nums = new int[][] {{0,0,3,5,0,2,9,0,0},
+                            {0,0,0,0,4,0,0,0,0},
+                            {1,0,6,0,0,0,3,0,5},
+                            {9,0,0,2,5,1,0,0,8},
+                            {0,7,0,4,0,8,0,3,0},
+                            {8,0,0,7,6,3,0,0,1},
+                            {3,0,8,0,0,0,1,0,4},
+                            {0,0,0,0,2,0,0,0,0},
+                            {0,0,5,1,0,4,8,0,0}};
         
+        nums = SudokuReader.test();
+        
+        //sample hard
+        /*
+        nums = new int[][]{{0,0,1,0,0,7,0,9,0},
+                           {5,9,0,0,8,0,0,0,1},
+                           {0,3,0,0,0,0,0,8,0},
+                           {0,0,0,0,0,5,8,0,0},
+                           {0,5,0,0,6,0,0,2,0},
+                           {0,0,4,1,0,0,0,0,0},
+                           {0,8,0,0,0,0,0,3,0},
+                           {1,0,0,0,2,0,0,7,9},
+                           {0,2,0,7,0,0,4,0,0}};
+        */
         
 
         grid.setGrid(nums);
         
         grid.initialize();
-        levelOneLogic();
+        levelTwoLogic();
         
         grid.printGrid();
         System.out.println("");
@@ -63,8 +77,11 @@ public class SudokuSolver
         
     }
     
-    public static void levelOneLogic()
+    
+    
+    public static boolean levelOneLogic()
     {
+        boolean levelTwoNotDone = false;
         boolean notDone = true;
         while(notDone)
         {
@@ -78,9 +95,65 @@ public class SudokuSolver
                     {
                         temp.set(temp.listPotentials().get(0));
                         notDone = true;
+                        levelTwoNotDone = true;
                     }
                 }
         }
+        return levelTwoNotDone;
+    }
+    
+    public static void levelTwoLogic()
+    {
+        boolean notDone = true;
+        grid.printGrid();
+        while(notDone)
+        {
+            notDone = false;
+            
+            for(int i = 0; i<9; i++)
+            {
+                if(l2CheckGroup(grid.getRow(i)))
+                    notDone = true;
+                if(l2CheckGroup(grid.getCol(i)))
+                    notDone = true;
+                if(l2CheckGroup(grid.getSquare(i)))
+                    notDone = true;
+            }
+            
+            if(levelOneLogic())
+                notDone = true;
+        }
+    }
+    
+    public static boolean l2CheckGroup(SuGroup sg)
+    {
+        boolean notDone = false;
+        
+        //for each number
+        for(int i = 0; i<9; i++)
+        {
+            //if only one cell can be that number, it must be that cell.
+            int onlyOne = -1;
+            for(int j = 0; j<9; j++)
+            {
+                //if it's the first, we want to remember it. If there's been others, we don't want anything.
+                if(sg.getCell(j).getPotentials()[i])
+                {
+                    if(onlyOne == -1)
+                        onlyOne = j;
+                    else
+                        onlyOne = -2;
+                }
+            }
+            if(onlyOne > -1)
+            {
+                    sg.getCell(onlyOne).set(i+1);
+                    notDone = true;
+            }
+            
+        }
+        
+        return notDone;
     }
     
     
