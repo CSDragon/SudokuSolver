87983e334924a9a15e63f7c90095eddff994cfee
CSDragon <CSDragon31@hotmail.com>
2018-08-02 08:29:56 -0500

    prep for logic level 3

--- src/solenus/sudokusolver/SudokuSolver.java
+++ src/solenus/sudokusolver/SudokuSolver.java
@@ -75,6 +75,10 @@ public class SudokuSolver
             System.out.println("");
         }
         
+        for(int i = 0; i<9; i++)
+            for(int j = 0; j<9; j++)
+                grid.getCell(i, j).printCell();
+        
     }
     
     
